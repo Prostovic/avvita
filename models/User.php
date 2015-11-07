d96aa178acd24eb4c5d88b70e4bfe1d26cde6f91
Vic <452@list.ru>
2015-11-08 00:14:57 +0300

    Changes in user register form

--- models/User.php
+++ models/User.php
@@ -31,6 +31,7 @@ use Yii;
  */
 class User extends \yii\db\ActiveRecord
 {
+    public $isAgree;
     /**
      * @inheritdoc
      */
@@ -45,8 +46,8 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['us_fam', 'us_name', 'us_email', 'us_adr_post', 'us_birth', 'us_pass', 'us_position', 'us_city', 'us_org', 'us_getnews', 'us_getstate', 'isAgree', ], 'required'],
             [['us_active', 'us_position', 'us_getnews', 'us_getstate'], 'integer'],
-            [['us_fam', 'us_name', 'us_otch', 'us_email', 'us_adr_post', 'us_birth', 'us_pass', 'us_position', 'us_city', 'us_org', 'us_created'], 'required'],
             [['us_birth', 'us_created', 'us_confirm', 'us_activate'], 'safe'],
             [['us_fam', 'us_name', 'us_otch'], 'string', 'max' => 32],
             [['us_email'], 'string', 'max' => 64],
@@ -67,14 +68,14 @@ class User extends \yii\db\ActiveRecord
             'us_fam' => 'Фамилия',
             'us_name' => 'Имя',
             'us_otch' => 'Отчество',
-            'us_email' => 'Электронная почта',
+            'us_email' => 'Email',
             'us_phone' => 'Телефон',
             'us_adr_post' => 'Адрес доставки',
             'us_birth' => 'Дата рождения',
             'us_pass' => 'Пароль',
             'us_position' => 'Специализация',
             'us_city' => 'Город',
-            'us_org' => 'Название организации',
+            'us_org' => 'Организация',
             'us_city_id' => 'id города',
             'us_org_id' => 'id организации',
             'us_created' => 'Создан',
@@ -83,6 +84,7 @@ class User extends \yii\db\ActiveRecord
             'us_getnews' => 'Инф. об акциях',
             'us_getstate' => 'Инф. о бонусах',
             'us_group' => 'Группа',
+            'isAgree' => 'Согласие на обработку',
         ];
     }
 
@@ -103,7 +105,17 @@ class User extends \yii\db\ActiveRecord
                 'us_org',
                 'us_getnews',
                 'us_getstate',
+                'isAgree',
             ],
+        ];
+    }
+
+    public function getPositions() {
+        return [
+            1 => 'Врач',
+            2 => 'Оптометрист',
+            3 => 'Продавец-консультант',
+            4 => 'Другое',
         ];
     }
 }
--- views/user/_formreg.php
+++ views/user/_formreg.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
@@ -13,11 +14,13 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin([
         'id' => 'register-form',
         'options' => [
-            'class' => 'form-horizontal'
+//            'class' => 'form-horizontal'
         ],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-md-10\">{input}</div>\n<div class=\"col-md-offset-2 col-md-10\">{error}</div>",
-            'labelOptions'=>['class'=>'control-label col-md-2'],
+//            'template' => "{label}\n<div class=\"col-md-6\">{input}</div>\n<div class=\"col-md-offset-6 col-md-6\">{error}</div>",
+            'template' => "{input}\n{error}",
+            'options' => ['class' => 'form-group col-md-3'],
+            'labelOptions'=>['class'=>'control-label col-md-6'],
         ],
         'enableAjaxValidation' => true,
         'enableClientValidation' => false,
@@ -27,41 +30,60 @@ use yii\widgets\ActiveForm;
         'validateOnSubmit' => true,
     ]); ?>
 
-    <?= $form->field($model, 'us_fam')->textInput(['maxlength' => true]) ?>
+    <!-- div class="width1_3">
+    </div -->
+    <?= $form->field($model, 'us_fam')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_fam')]) ?>
 
-    <?= $form->field($model, 'us_name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'us_name')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_name')]) ?>
 
-    <?= $form->field($model, 'us_otch')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'us_otch')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_otch')]) ?>
 
-    <?= $form->field($model, 'us_email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'us_position')->dropDownList($model->getPositions(), ['placeholder' => $model->getAttributeLabel('us_position')]) ?>
 
-    <?= $form->field($model, 'us_phone')->textInput(['maxlength' => true]) ?>
+    <div class="clearfix"></div>
 
-    <?= $form->field($model, 'us_adr_post')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'us_email')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_email')]) ?>
 
-    <?= $form->field($model, 'us_birth')->textInput() ?>
+    <?= $form->field($model, 'us_pass')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_pass')]) ?>
 
-    <?= $form->field($model, 'us_pass')->textInput(['maxlength' => true]) ?>
+    <?= '' // $form->field($model, 'us_phone')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_phone')]) ?>
+    <?= $form->field($model, 'us_phone')->widget(MaskedInput::classname(), [
+        'mask' => '+7(999)999-99-99',
+        'options' => ['placeholder' => $model->getAttributeLabel('us_phone'), 'class' => 'form-control', ],
+    ]) ?>
 
-    <?= $form->field($model, 'us_position')->textInput() ?>
+    <?= '' // $form->field($model, 'us_birth', ['options' => ['class' => 'form-group col-md-3'],])->textInput(['placeholder' => $model->getAttributeLabel('us_birth')]) ?>
+    <?= $form->field($model, 'us_birth')->widget(MaskedInput::classname(), [
+        'mask' => '99.99.9999',
+        'options' => ['placeholder' => $model->getAttributeLabel('us_birth'), 'class' => 'form-control', ],
+    ]) ?>
 
-    <?= $form->field($model, 'us_city')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'us_org')->textInput(['maxlength' => true]) ?>
+    <div class="clearfix"></div>
+
+    <?= $form->field($model, 'us_city')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_city')]) ?>
+
+    <?= $form->field($model, 'us_org')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_org')]) ?>
+
+    <?= $form->field($model, 'us_adr_post', ['options' => ['class' => 'form-group col-md-6'],])->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('us_adr_post')]) ?>
 
     <?php if( false ): ?>
 
-        <?= $form->field($model, 'us_city_id')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'us_city_id')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('')]) ?>
 
-        <?= $form->field($model, 'us_org_id')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'us_org_id')->textInput(['maxlength' => true, 'placeholder' => $model->getAttributeLabel('')]) ?>
 
     <?php endif; ?>
+
+    <div class="clearfix"></div>
 
     <?= $form->field($model, 'us_getnews')->textInput() ?>
 
     <?= $form->field($model, 'us_getstate')->textInput() ?>
 
-    <div class="form-group">
+    <div class="clearfix"></div>
+
+    <div class="form-group col-md-4">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
